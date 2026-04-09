f39b86ccb03d7f49237e8ab8df1282e0812838dc
chibuikemkingsley7-glitch <chibuikemkingsley7@gmail.com>
2026-04-09 12:49:11 +0100

    Update Agent.js

--- models/Agent.js
+++ models/Agent.js
@@ -1,6 +1,16 @@
-package.json
-server.js
-.env (optional for now)
-models/Agent.js
-models/Customer.js
-models/Booking.js
+const mongoose = require('mongoose');
+
+const AgentSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  address: String,
+  service: String,
+  experience: Number,
+  profilePic: String,
+  kitPic: String,
+  purpleStar: Boolean,
+  status: { type: String, default: 'pending' } // pending/verified
+});
+
+module.exports = mongoose.model('Agent', AgentSchema);
